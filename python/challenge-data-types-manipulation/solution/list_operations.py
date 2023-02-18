915fe4e7e3180b12f31335e319bde5b11a4ccd9b
huangshiqi99 <125269378+huangshiqi99@users.noreply.github.com>
2023-02-18 16:21:46 +0800

    Update list_operations.py

--- python/challenge-data-types-manipulation/solution/list_operations.py
+++ python/challenge-data-types-manipulation/solution/list_operations.py
@@ -1,10 +1,5 @@
 def list_operations(input_list: list) -> list:
     """
-    This function performs the following operations on the input list:
-    1. Sort the list in ascending order.
-    2. Remove duplicates from the list.
-    3. Return the final list.
-    
     Args:
     input_list: a list of integers to be manipulated
     
